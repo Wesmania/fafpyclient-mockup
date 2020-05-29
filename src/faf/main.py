34e7b1fc95a4d01e6b3f32f32360a405d40c4b6a
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2020-05-29 20:55:30 +0200

    Put stuff initialization in a class
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- src/faf/main.py
+++ src/faf/main.py
@@ -23,21 +23,29 @@ from faf.irc import Irc
 from faf.environment import Environment
 
 
+class FAFClient:
+    def __init__(self, env):
+        self.env = env
+        self.resources = Resources(self.env.config["resources"],
+                                   self.env.aiohttp_session)
+        self.lobby_server = LobbyServer(self.env.config["lobby_server"],
+                                        self.env.tools)
+        self.irc = Irc(self.env.config["irc_server"])
+
+        self.models = Models(self.env.config, self.lobby_server, self.irc)
+        self.qt_models = QtModels(self.models, self.resources)
+
+        self.login_session = LoginSession(self.lobby_server, self.irc,
+                                          self.models, self.env.qml_engine_ctx)
+
+        self.news_tab = NewsTab(self.login_session, self.env.qml_engine_ctx)
+        self.games_tab = GamesTab(self.qt_models, self.env.qml_engine_ctx)
+        self.chat_tab = ChatTab(self.irc, self.models, self.env.qml_engine_ctx)
+
+
 def main():
     env = Environment(sys.argv)
-
-    lobby_server = LobbyServer(env.config["lobby_server"], env.tools)
-    irc = Irc(env.config["irc_server"])
-    resources = Resources(env.config["resources"], env.aiohttp_session)
-
-    models = Models(env.config, lobby_server, irc)
-
-    login_session = LoginSession(lobby_server, irc, models, env.qml_engine_ctx)
-    qt_models = QtModels(models, resources)
-
-    news = NewsTab(login_session, env.qml_engine_ctx)
-    games = GamesTab(qt_models, env.qml_engine_ctx)
-    chat = ChatTab(irc, models, env.qml_engine_ctx)
+    client = FAFClient(env)
 
     qml_file = os.path.join(env.paths.ROOT_PATH,
                             "res/ui/main_window/ToplevelWindow.qml")
